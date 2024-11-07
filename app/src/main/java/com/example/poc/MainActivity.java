d7ee4788973ac1c222d84e2a7bb0014334662bc6
Magshimim <ac8274@bs.amalnet.k12.il>
2024-11-07 10:40:21 +0200

    added first view

--- app/src/main/java/com/example/poc/MainActivity.java
+++ app/src/main/java/com/example/poc/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.poc;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +9,19 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth auth;
+    private Button Submit_bt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        auth = FirebaseAuth.getInstance();
+        FirebaseUser f = auth.getCurrentUser();
     }
 }
